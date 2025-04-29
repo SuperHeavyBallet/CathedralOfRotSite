9fab628cff14094f36bcd84a53f8fe64922013e9
Alexander Gorham <agram@hotmail.co.uk>
2025-04-29 22:47:43 +0200

    JSON / JS setup

--- js.js
+++ js.js
@@ -1,1 +1,12 @@
-console.log("Hello Cathedral")
+document.addEventListener("DOMContentLoaded", function() {
+    
+    let archivesData = [];
+
+    fetch('../content/archives.json')
+    .then(response => response.json())
+    .then(data => {
+        archivesData = data;
+        console.log(archivesData);
+    });
+
+});
