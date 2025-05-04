a09d18d291387addb2377b925a6091d12f170412
Alexander Gorham <agram@hotmail.co.uk>
2025-05-04 21:47:32 +0200

    Content + Copyright Autoupdate

--- javascript/common.js
+++ javascript/common.js
@@ -1,4 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    let copyrightText = document.getElementById("copyright-text");
+    let baseCopyrightText = "Copyright © 2025 Alexander Gorham. All content and associated intellectual property are protected."
+    copyrightText.textContent = baseCopyrightText;
     
+    
+
+    CopyrightUpdateGenerator();
+
+    function CopyrightUpdateGenerator()
+    {
+        let currentYear;
+        const date = new Date();
+        let year = date.getFullYear();
+        currentYear = year.toString();
+        copyrightText.textContent = `Copyright ${year} Alexander Gorham. All content and associated intellectual property are protected.`;
+    }
 });
