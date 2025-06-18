5a24d471ea060d03a0e0d28d98610eea42d017df
Alexander Gorham <agram@hotmail.co.uk>
2025-06-18 23:34:49 +0200

    New content entry

---
+++ javascript/index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    let randomEntry;
+    const entryHolder = document.getElementById("entryHolder");
+    const entryTitle = document.getElementById("entryTitle");
+    const entryText = document.getElementById("entryText");
+
+})
