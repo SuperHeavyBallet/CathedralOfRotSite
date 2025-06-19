0a95fbcba38e4aa09d0df1055181b25608026c0e
Alexander Gorham <agram@hotmail.co.uk>
2025-06-19 23:08:17 +0200

    Index archive generator + CSS

--- javascript/index.js
+++ javascript/index.js
@@ -4,5 +4,41 @@ document.addEventListener("DOMContentLoaded", function(){
     const entryHolder = document.getElementById("entryHolder");
     const entryTitle = document.getElementById("entryTitle");
     const entryText = document.getElementById("entryText");
+    let archivesData;
+
+    FetchArchivesContent();
+
+
+
+    function FetchArchivesContent()
+    {
+        fetch('./content/archives.json')
+        .then(response => response.json())
+        .then(data => {
+            archivesData = data;
+            
+      
+
+            ShowRandomArchive();
+
+
+        });
+    }
+
+    function ShowRandomArchive()
+    {
+        let randomInt = 0;
+        let maxRange = archivesData.length;
+
+        randomInt = Math.floor(Math.random() * maxRange);
+
+        let chosenArchive = archivesData[randomInt];
+        
+
+        entryTitle.textContent = chosenArchive.title;
+        entryText.textContent = chosenArchive.content[0];
+    
+
+    }
 
 })
